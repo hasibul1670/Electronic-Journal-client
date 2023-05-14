52fc9b6074509cf0c44d4b912de811e98504c87c
hasibul1670 <hasibul1670@gmail.com>
2023-05-14 10:04:17 +0600

    yguhgj

--- src/Components/Admin/UpdateProfile.js
+++ src/Components/Admin/UpdateProfile.js
@@ -1,6 +1,8 @@
-import { PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Upload } from "antd";
+import { Button, Form, Input } from "antd";
+import React from "react";
+
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Toaster, toast } from "react-hot-toast";
 import { useQuery } from "react-query";
 import Loading from "../Shared/Loading";
@@ -23,177 +25,152 @@ const UpdateProfile = ({ user }) => {
     },
   });
 
-  const handleValuesChange = (changedValues, allValues) => {
-    setFormData(allValues, changedValues);
-  };
-  
   const imageHostKey = process.env.REACT_APP_imgbb_key;
-
-
-const uploadImage =()=>{
-  const image1 = formData?.profilePic?.file;
-  console.log(image1);
-  const imageData = new FormData();
-  imageData.append("image", image1);
-  const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`;
-
-fetch (url,{
-  method: 'POST',
-  body :imageData
-})
-.then(res => res.json())
-.then(iData => {
-        console.log(iData.data);   
-        }
-)}
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
 
 
 
 
+  const uploadImage = (event) => {
+    const selectedFile = event.target.files[0];
+    if (!selectedFile) {
+      return;
+    }
+    const iData = new FormData();
+    iData.append("image", selectedFile);
+    const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`;
+
+    fetch(url, {
+      method: "POST",
+      body: iData,
+    })
+      .then((res) => res.json())
+      .then((iData) => {
+        setImageUrl(iData.data.display_url);
+      });
+  };
+
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const handleValuesChange = (changedValues, allValues) => {
+    setFormData(allValues,changedValues);
+//   console.log("Hello", formData);
+ };
+  
   const handleSubmit = async () => {
-    uploadImage();
     try {
-
-
-      const formDataWithFile = new FormData();
-      formDataWithFile.append("authorName", formData.authorName);
-      formDataWithFile.append("phone", formData.phone);
-      formDataWithFile.append("institutionName", formData.institutionName);
-      formDataWithFile.append("department", formData.department);
-      formDataWithFile.append("city", formData.city);
-      formDataWithFile.append("postalCode", formData.postalCode);
-      formDataWithFile.append("profilePic", formData.profilePicFile);
-
       const response = await fetch(
-        //`http://localhost:4000/authorData/${user?.email}`,
+       `http://localhost:4000/authorData/${user?.email}`,
         {
           method: "PUT",
-          body: formDataWithFile,
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
       );
       const data = await response.json();
       console.log(data.modifiedCount);
-
-      if (data.modifiedCount > 0) {
+      if (data.modifiedCount>0){
         toast.success("Update Data Successfully");
         refetch();
-      } else {
+      }else{
         toast.error("Nothing To Update");
       }
+
+
     } catch (error) {
       console.error(error);
       toast.error("Update Data Error");
-    }
-  };
+   }
+  }
 
   if (isLoading) {
     return <Loading />;
   }
 
-  const beforeUpload = (file) => {
-    if (file.size > 5 * 1024 * 1024) {
-      toast.error("File must be smaller than 5MB.");
-      return false;
-    }
-    if (user?.profilePic) {
-      toast.error("You can only upload one file.");
-      return false;
-    }
-    return true;
-  };
-
   return (
     <div className="container-fluid">
+      <img
+        src={users[0].profilePic}
+        alt="Imafgfgge"
+        style={{ width: "300px", height: "300px" }}
+      />
       {users &&
         users?.map((user) => (
           <div key={user?._id} className="">
+
+
+       
             <Form
               name="basic"
-              labelCol={{ span: 32 }}
-              wrapperCol={{ span: 32 }}
               initialValues={{ remember: true }}
               onValuesChange={handleValuesChange}
             >
-              <div className="d-flex">
-                <div>
+              {" "}
+              <Form.Item
+                label="Name"
+                name="authorName"
+                initialValue={user?.authorName}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item label="Phone" name="phone" initialValue={user?.phone}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Institution Name"
+                name="institutionName"
+                initialValue={user?.institutionName}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Department"
+                name="department"
+                initialValue={user?.department}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item label="City" name="city" initialValue={user?.city}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Postal Code"
+                name="postalCode"
+                initialValue={user?.postalCode}
+              >
+                <Input />
+              </Form.Item>
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
                   {" "}
-                  <Form.Item
-                    label="Name"
-                    name="authorName"
-                    initialValue={user?.authorName}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    label="Phone"
-                    name="phone"
-                    initialValue={user?.phone}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    label="Institution Name"
-                    name="institutionName"
-                    initialValue={user?.institutionName}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    label="Department"
-                    name="department"
-                    initialValue={user?.department}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item label="City" name="city" initialValue={user?.city}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    label="Postal Code"
-                    name="postalCode"
-                    initialValue={user?.postalCode}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-                    <Button type="primary" onClick={handleSubmit}>
-                      Save
-                    </Button>
-                  </Form.Item>
-                </div>
-                <div className="ml-5">
-                  <Form.Item
-                    label="Profile Picture"
-                    name="profilePic"
-                    initialValue={user?.profilePic}
-                  >
-                    <Upload
-                      name="profilePic"
-                      listType="picture-card"
-                      beforeUpload={beforeUpload}
-                       //onChange={uploadImage}
-                    >
-                      {user?.profilePic ? (
-                        // eslint-disable-next-line jsx-a11y/img-redundant-alt
-                        <img
-                          src={user.profilePic}
-                          alt="Profile Picture"
-                          style={{
-                            width: "300px",
-                            height: "300px",
-                            objectFit: "cover",
-                          }}
-                        />
-                      ) : (
-                        <div>
-                          <PlusOutlined />
-                          <div style={{ marginTop: 8 }}>Upload</div>
-                        </div>
-                      )}
-                    </Upload>
-                  </Form.Item>
-                </div>
+                  <span className="label-text">Upload Your Profile Photo</span>
+                </label>
+                <input
+                  type="file"
+                  {...register("image", {
+                    required: "Photo is Required",
+                  })}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={uploadImage}
+                />
+                {errors.img && (
+                  <p className="text-red-500">{errors.img.message}</p>
+                )}
               </div>
+              {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+
+
+
+              <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
+                <Button type="primary" onClick={handleSubmit}>
+                  Save
+                </Button>
+              </Form.Item>
             </Form>
           </div>
         ))}
--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -1,50 +1,10 @@
+
 import React from 'react';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
 
 const Test = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const imageHostKey = process.env.REACT_APP_imgbb_key;
-  const [imageUrl, setImageUrl] = useState(null);
-
-
-  const uploadImage = (event) => {
-    const selectedFile = event.target.files[0];
-    if (!selectedFile) {
-      return;
-    }
-    const formData = new FormData();
-    formData.append("image", selectedFile);
-    const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`;
-  
-    fetch(url, {
-      method: 'POST',
-      body: formData
-    })
-    .then(res => res.json())
-    .then(iData => {
-      console.log(iData.data.display_url);
-      setImageUrl(iData.data.url);
-    })
-  }
-
   return (
     <div>
-      <form className="form-control w-full max-w-xs">
-        <label className="label"> <span className="label-text">Photo</span></label>
-        <input
-          type="file"
-          {...register("image", {
-            required: "Photo is Required"
-          })}
-          className="input input-bordered w-full max-w-xs"
-          onChange={uploadImage}
-        />
-        {errors.img && <p className='text-red-500'>{errors.img.message}</p>}
-      </form>
-      <input onClick={handleSubmit(uploadImage)} className='btn btn-accent w-full mt-4' value="Add Doctor" type="submit" />
-      {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+      
     </div>
   );
 };
